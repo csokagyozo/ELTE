ad956178ccd7bec6ca940dbb397206c8b1962c64
Győző Csóka <csokagyozo@gmail.com>
2023-09-19 14:50:54 +0200

    tippelos feladat hozzaadva

---
+++ programozas-23-24-1/gyakorlat02/Tippelos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Tippelos
+{
+    static void Main(string[] args)
+    //public Tippelos()
+    {
+        Random rnd = new Random();
+        int szam = rnd.Next(1, 1001);
+
+        //Console.WriteLine("{0}", szam);
+
+        int tippekSzama = 0;
+        int tipp = 0;
+        
+        //TODO: algoritmus
+
+        Console.WriteLine("Kitaláltad, {0} tippből", tippekSzama);
+
+    }
+}
