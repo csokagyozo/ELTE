25cae4f77e17ec1c2343164fa8c6df69b51dc130
Győző Csóka <csokagyozo@gmail.com>
2023-10-10 12:42:13 +0200

    5. gyakorlat peldak

---
+++ programozas-23-24-1/gyakorlat05/Kapas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Kapas
+{
+	public Kapas()
+    //static void Main(string[] args)
+    {
+        // Deklaráció: spec be, ki
+        int n;
+        int[] fogasok_szama;
+        bool van;
+        int ind;
+
+        // Beolvasás
+        Console.Error.Write("Hány nap: ");
+        int.TryParse(Console.ReadLine(), out n);
+        fogasok_szama = new int[n];
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Error.Write("{0}. napi fogások száma: ", i);
+            int.TryParse(Console.ReadLine(), out fogasok_szama[i - 1]);
+        }
+
+        // Feldolgozás:
+        ind = 1;
+        van = false;
+        while (!van && ind <= n)
+        {
+            if (fogasok_szama[ind - 1] == 0)
+            {
+                van = true;
+            }
+            else
+            {
+                ind++;
+            }
+        }
+
+        // Kiírás
+        Console.Error.Write("Az első nap fogás nélkül: ");
+        Console.WriteLine(van ? ind : -1);
+    }
+}
---
+++ programozas-23-24-1/gyakorlat05/NevsorbanNovekvoMagassag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class NevsorbanNovekvoMagassag
+{
+	public NevsorbanNovekvoMagassag()
+    //static void Main(string[] args)
+    {
+        // Deklaráció: spec be, ki
+        int n;
+        int[] magassag;
+        bool mind;
+
+        // Beolvasás
+        Console.Error.Write("Hány tanuló: ");
+        int.TryParse(Console.ReadLine(), out n);
+        magassag = new int[n];
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Error.Write("{0}. tanuló neve, magassága : ", i);
+            String sor = Console.ReadLine();
+            magassag[i - 1] = int.Parse(sor.Split(" ")[1]);
+        }
+
+        // Feldolgozás:
+        int ind = 1;
+        mind = true;
+        while (ind < n && magassag[ind - 1] <= magassag[ind])
+        {
+            ind++;
+        }
+        mind = ind == n;
+
+        // Kiírás
+        Console.Error.Write("Magasság szerint is növekvő sorban vannak: ");
+        Console.WriteLine(mind ? "IGEN" : "NEM");
+    }
+}
