0f42927e6ee169c56966b6556e7ca273357f2064
Győző Csóka <csokagyozo@gmail.com>
2023-10-17 21:13:55 +0200

    6. gyakorlat feladatok, megoldások

--- programozas-23-24-1/gyakorlat06/SzabadHelyek.cs
+++ programozas-23-24-1/gyakorlat06/SzabadHelyek.cs
@@ -9,11 +9,64 @@ public class SzabadHelyek
     static void Main(string[] args)
     {
         // Deklaráció: spec be, ki
+        int n;
+        int m;
+        int[,] helyek;
+        bool vanSzomszedos = false;
 
         // Beolvasás
+        String sor = Console.ReadLine();
+        n = int.Parse(sor.Split(" ")[0]);
+        m = int.Parse(sor.Split(" ")[1]);
+        helyek = new int[n,2*m];
+
+        for (int i = 0; i < n; ++i)
+        {
+            sor = Console.ReadLine();
+            String[] balsor = new String[m];
+            balsor = sor.Split(" ");
+            
+            for (int j = 0; j < m; j++)
+            {
+                helyek[i,j] = int.Parse(balsor[j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            sor = Console.ReadLine();
+            String[] jobbsor = new String[m];
+            jobbsor = sor.Split(" ");
+
+            for (int j = 0; j < m; j++)
+            {
+                helyek[i,2*m - j - 1] = int.Parse(jobbsor[j]);
+            }
+        }
+
+        // Ellenorzes:
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < 2 * m; j++)
+            {
+                Console.Error.Write(helyek[i, j]);
+            }
+            Console.Error.Write("\n");
+        }
 
         // Feldolgozas
+                for (int i = 0; !vanSzomszedos && i < n; i++)
+        {
+            for (int j = 0; !vanSzomszedos && j < 2*m-1; j++)
+            {
+                if (helyek[i,j] == 0 && helyek[i,j+1] == 0)
+                {
+                    vanSzomszedos = true;
+                }
+            }
+        }
 
         // Kiiras
+        Console.Write("{0}", vanSzomszedos ? "IGEN" : "NEM");
     }
 }
